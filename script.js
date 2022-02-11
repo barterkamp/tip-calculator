866ca2be97a8acfd160dff9cb7c80bc2754b57c6
barterkamp <84913017+barterkamp@users.noreply.github.com>
2022-02-11 18:08:45 +0100

    added raw calculation to script

--- script.js
+++ script.js
@@ -2,24 +2,39 @@
 
 const billInput = document.querySelector("#bill");
 const peopleInput = document.querySelector("#people");
-const button5 = document.querySelector(".num5");
+const button15 = document.querySelector(".num15");
 const tipAmount = document.querySelector(".tip-amount");
 const tipPerPerson = document.querySelector(".total-person");
-
-const error = document.querySelector(".calc__title—error");
+const error = document.querySelector(".calc__people—error");
+const resetBtn = document.querySelector(".btn--reset");
 
 // schrijf een functie die de berekning uitvoert op basis van
 // gegevens uit illInput en peopleInput.
 
-button5.addEventListener("click", function () {
+// eventlistener op 10% button
+button15.addEventListener("click", function () {
+  // check of er een input is en of de input een number is
   if (Number(peopleInput.value) === 0 || !peopleInput.value) {
+    // error als niet aan de bovenstaande voorwaardem is voldaan
     error.style.display = "block";
+    peopleInput.style.border = "2px solid red";
     return;
   } else {
     error.style.display = "none";
+    peopleInput.style.border = "none";
   }
-  const totalAmount = billInput.value * 0.05;
-  tipAmount.textContent = totalAmount;
-  const totalPerPerson = totalAmount / peopleInput.value;
+  // bereken tip amount
+  const totalTipAmount = (billInput.value * 0.15) / peopleInput.value;
+
+  tipAmount.textContent = totalTipAmount;
+  // bereken totaalbedrag
+  const totalPerPerson = parseFloat(
+    billInput.value / peopleInput.value + totalTipAmount
+  ).toFixed(2);
+
   tipPerPerson.textContent = totalPerPerson;
 });
+
+// const totalPerPerson = billInput.value / totalAmount;
+// tipPerPerson.textContent = totalPerPerson;
+// resetBtn.style.backgroundColor = "#26C2AE";
