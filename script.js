d576e27585274c815ef675d5d460a4ca9a2c80a4
barterkamp <84913017+barterkamp@users.noreply.github.com>
2022-02-14 13:57:04 +0100

    Added calculator fucntion to script

--- script.js
+++ script.js
@@ -2,39 +2,49 @@
 
 const billInput = document.querySelector("#bill");
 const peopleInput = document.querySelector("#people");
-const button15 = document.querySelector(".num15");
+const btnPercentage = document.querySelectorAll(".btn--primary");
 const tipAmount = document.querySelector(".tip-amount");
 const tipPerPerson = document.querySelector(".total-person");
 const error = document.querySelector(".calc__people—error");
 const resetBtn = document.querySelector(".btn--reset");
+const custom = document.querySelector("#custom");
 
-// schrijf een functie die de berekning uitvoert op basis van
-// gegevens uit illInput en peopleInput.
+console.log(custom);
 
-// eventlistener op 10% button
-button15.addEventListener("click", function () {
-  // check of er een input is en of de input een number is
-  if (Number(peopleInput.value) === 0 || !peopleInput.value) {
-    // error als niet aan de bovenstaande voorwaardem is voldaan
-    error.style.display = "block";
-    peopleInput.style.border = "2px solid red";
-    return;
-  } else {
-    error.style.display = "none";
-    peopleInput.style.border = "none";
-  }
-  // bereken tip amount
-  const totalTipAmount = (billInput.value * 0.15) / peopleInput.value;
+btnPercentage.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    // check if there en of de input een number is
+    if (Number(peopleInput.value) === 0 || !peopleInput.value) {
+      error.style.display = "block";
+      peopleInput.style.border = "2px solid red";
+      return;
+    } else {
+      error.style.display = "none";
+      peopleInput.style.border = "none";
 
-  tipAmount.textContent = totalTipAmount;
-  // bereken totaalbedrag
-  const totalPerPerson = parseFloat(
-    billInput.value / peopleInput.value + totalTipAmount
-  ).toFixed(2);
+      //calculate percentage per button
+      const percentage = Number(e.target.dataset.num) / 100;
 
-  tipPerPerson.textContent = totalPerPerson;
+      // calculate total tip amount per person
+
+      const totalTipAmount = (billInput.value * percentage) / peopleInput.value;
+      tipAmount.textContent = `$${totalTipAmount.toFixed(2)}`;
+
+      // calculate Total amount per person
+      const totalPerPerson =
+        billInput.value / peopleInput.value + totalTipAmount;
+      tipPerPerson.textContent = `$${totalPerPerson.toFixed(2)}`;
+
+      resetBtn.style.backgroundColor = "#26C2AE";
+    }
+  });
 });
 
-// const totalPerPerson = billInput.value / totalAmount;
-// tipPerPerson.textContent = totalPerPerson;
-// resetBtn.style.backgroundColor = "#26C2AE";
+//Reset functionality
+resetBtn.addEventListener("click", function () {
+  tipAmount.textContent = "$0.00";
+  tipPerPerson.textContent = "$0.00";
+  billInput.value = "";
+  peopleInput.value = "";
+  resetBtn.style.backgroundColor = "#0d686d";
+});
